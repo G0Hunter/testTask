1fe3766536abb1f3ef94f80b48c7de1c44c28a00
G0Hunter <kondrashkin403@mail.ru>
2017-03-09 15:26:16 +0200

    Update task1.js

--- task1.js
+++ task1.js
@@ -1,15 +1,29 @@
-var menu = new Menu({
-  title: "Сладости",
-  // передаём также шаблоны
-  template: _.template(document.getElementById('menu-template').innerHTML),
-  listTemplate: _.template(document.getElementById('menu-list-template').innerHTML),
-  items: [
-    "Торт",
-    "Пончик",
-    "Пирожное",
-    "Шоколадка",
-    "Мороженое"
-  ]
-});
+function def(some){
+      return some !== undefined && some !== null
+}
+console.log(def());
+/////////
+ Node.prototype.attach = function(node) {
+            return this.appendChild(node);
+ }
+console.log(document.getElementById('list').attach(someNode));
+\\\\\\\\\\
+ Node.prototype.detach = function() {
+           return this.parentNode.removeChild(this)
+ }
+console.log(document.getElementById('k').detach());
+\\\\\\\\\\\\\\\
+Node.prototype.getChildren = function() {
+var arrNode = [];
+for (var i = 0; i < this.childNodes.length; i++) {
+      if (this.childNodes[i].nodeType == 1)
+      arrNode.push(this.childNodes[i]);
+    }
+    return arrNode;
+}
+console.log(document.getElementById('list').getChildren());
+\\\\\\\\\\\\\\\
 
-document.body.appendChild(menu.getElem());
+\\\\\\\\\\\\\\\
+
+\\\\\\\\\\\\\\\
