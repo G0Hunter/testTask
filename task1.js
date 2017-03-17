cc1a66b8714be588e2155af66f32c3d9888066f6
G0Hunter <kondrashkin403@mail.ru>
2017-03-17 20:17:08 +0200

    Update task1.js

--- task1.js
+++ task1.js
@@ -34,3 +34,27 @@ Node.prototype.insBefore = function(node) {
   		      return this.parentNode.insertBefore(node, this);
 }
 \\\\\\\\\\\\\\\
+
+function ajax_get(url) {
+    xmlhttp = new XMLHttpRequest();
+    var global= [];
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4) {
+            var result =JSON.parse(xmlhttp.responseText);
+            var fields = result.fields[0];
+            var data = result.data;
+            for(var j = 0; j < data.length; j++) {
+                var obj = {};
+                var dataCur = result.data[j];
+                for (var i = 0; i < dataCur.length; i++) {
+                    obj[fields[i+1]] = dataCur[i];
+                };
+                global.push(obj);
+            }
+        }
+    };
+    xmlhttp.open("GET", url);
+    xmlhttp.send();
+    return global;
+}
+ajax_get();
